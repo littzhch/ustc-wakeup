d15d040036fcee8b473ad666892555e15bc92a94
驰 张 <2371050115@qq.com>
2021-09-08 08:56:48 +0800

    add more features

--- XmlReader.py
+++ XmlReader.py
@@ -4,13 +4,14 @@ import re
 activity_match_regex = \
     r"[\s\S]*?<activities>[\s\S]*?"\
     r"<coursename>([\s\S]*?)</coursename>[\s\S]*?"\
-    r"<weeksstr>([0-9]*)-([0-9]*)([\S]*?)</weeksstr>"\
-    r"[\s\S]*?(?:(?:<room>([\s\S]*?)</room>)|(?:<\s*room\s*/\s*>))[\s\S]*?"\
+    r"<weeksstr>([0-9]*)(\S*?)(?:(,)|(?:</weeksstr>))"\
+    r"[\s\S]*?(?:(?:<room>([\s\S]*?)</room>)|(?:<room />))[\s\S]*?"\
     r"<weekday>([1-9])</weekday>[\s\S]*?"\
     r"<startunit>([0-9]*)</startunit>"\
     r"[\s\S]*?<endunit>([0-9]*)</endunit>[\s\S]*?"\
     r"<teachers>[\s]*<teachers>([\s\S]*?)</teachers>"\
     r"[\s\S]*?<credits>([\s\S]*?)</credits>"\
+    r"[\s\S]*?(?:(?:<customplace>([\s\S]*?)</customplace>)|(?:<customplace />))"\
     r"[\s\S]*?</activities>"
 rawstring = ""
 
@@ -23,23 +24,36 @@ def _read_single_activity():
         return None
 
     result["course_name"] = match_obj.group(1)
-    result["start_week"] = int(match_obj.group(2))
-    result["end_week"] = int(match_obj.group(3))
-    result["location"] = match_obj.group(5) if match_obj.group(5) else ""
+    result["location"] = \
+        match_obj.group(11) if match_obj.group(11) \
+        else (match_obj.group(5) if match_obj.group(5) else "")
     result["day"] = int(match_obj.group(6))
     result["start_period"] = int(match_obj.group(7))
     result["length"] = int(match_obj.group(8)) - result["start_period"] + 1
     result["teacher"] = match_obj.group(9)
     result["credit"] = float(match_obj.group(10))
 
-    if match_obj.group(4) == '双':
-        result["week_type"] = 2
-    elif match_obj.group(4) == '单':
-        result["week_type"] = 1
+    result["start_week"] = int(match_obj.group(2))
+    if match_obj.group(3):
+        if match_obj.group(3)[-1] == '双':
+            result["week_type"] = 2
+            result["end_week"] = int(match_obj.group(3)[1:-1])
+        elif match_obj.group(3)[-1] == '单':
+            result["week_type"] = 1
+            result["end_week"] = int(match_obj.group(3)[1:-1])
+        else:
+            result["week_type"] = 0
+            result["end_week"] = int(match_obj.group(3)[1:])
     else:
         result["week_type"] = 0
+        result["end_week"] = result["start_week"]
 
-    rawstring = rawstring[match_obj.span()[1]:]
+    if match_obj.group(4):
+        rawstring = re.sub(r"<weeksstr>[\S]*?,", "<weeksstr>", rawstring, count=1)
+    else:
+        rawstring = rawstring[match_obj.span()[1]:]
+
+    print(result)
     return result
 
 
@@ -47,7 +61,6 @@ def read_activities(rawstr: str):
     global rawstring
     rawstring = rawstr
     result = []
-    info_dict = {}
     start = re.search(r"</credits>\s*<activities>", rawstr).span()[1]
     rawstring = rawstring[start:]
     while True:
