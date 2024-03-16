0490395e1a9a7cb9458f919d3d85e9b443cfa6a7
littzhch <2371050115@qq.com>
2024-03-16 17:44:16 +0800

    定义后端接口

---
+++ src/ustcwakeup/backend/__init__.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+from datetime import date
+from ..course_table import *
+
+
+class CourseTableOutput(ABC):
+    @abstractmethod
+    def suggest_file_name(self) -> str:
+        """
+        不包含后缀名
+        """
+        pass
+    
+    @abstractmethod
+    def get_file_extension(self) -> str:
+        pass
+    
+    @abstractmethod
+    def get_content(self) -> bytes:
+        pass
+
+
+class CourseTableHandler(ABC):
+    @staticmethod
+    @abstractmethod
+    def backend_name() -> str:
+        pass
+    
+    @abstractmethod
+    def __init__(self, semester_name: str):
+        pass
+    
+    @abstractmethod
+    def add_course(self, course: Course):
+        pass
+    
+    @abstractmethod
+    def set_semester_start_date(self, start_date: date):
+        """
+        第一周周一的前一天
+        """
+        pass
+    
+    @abstractmethod
+    def set_semester_weeks(self, amount: int):
+        pass
+    
+    @abstractmethod
+    def finish(self) -> CourseTableOutput:
+        pass
+
+available_handlers: list[type[CourseTableHandler]] = []
+
+def register_handler(handler: type[CourseTableHandler]):
+    available_handlers.append(handler)
+    
+
+__all__ = ["CourseTableHandler", "CourseTableOutput", "register_handler", "available_handlers"]
+
